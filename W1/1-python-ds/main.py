e2336ad6ebb28e4322c913d84fa10c2afc428627
EdwardSc <dev.edwardsc@gmail.com>
2026-07-08 21:49:06 -0500

    intall package,view requirements

---
+++ W1/1-python-ds/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+vector = np.array([1, 2, 3, 4])
+
+matriz = np.array([
+    [1, 2],
+    [3, 4],
+    [5, 6]
+])
+
+print(vector.shape) # Indica que es un vector de n items
+print(matriz.shape) # Indica que es una matriz de nxn
